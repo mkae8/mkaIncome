f8614708208cc81d88e9f61cfec1534f4ff74179
24LP9087 24LP9087 <24LP9087@G302-CC0115.local>
2024-09-27 20:08:02 +0800

    S

--- server/controllers/user/login.js
+++ server/controllers/user/login.js
@@ -1,20 +1,34 @@
-import fs from "fs";
 import { getDataBase } from "../../utils/helper.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import env from "dotenv";
 
-export const login = async (res, req) => {
+env.config();
+
+export const login = async (req, res) => {
   const { email, password } = req.body;
   const dataBase = await getDataBase();
 
   const isUserExisting = dataBase.users.find((el) => el.email === email);
 
   if (!isUserExisting) {
-    res.send("Email not registered").status(400);
-    return;
+    return res.status(400).send({ message: "Email not registered" });
   }
-  //   const isMatch = await bcrypt.compare(password, user.password);
-  //   if (!isMatch) {
-  //     res.send("Password or email not matched").status(400);
-  //     return;
-  //   }
+
+  const isMatch = await bcrypt.compare(password, isUserExisting.password);
+  if (!isMatch) {
+    return res.status(400).send({ message: "Email or password is wrong" });
+  }
+
+  const token = jwt.sign(
+    { userId: isUserExisting.userId },
+    process.env.SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+
+  return res
+    .status(200)
+    .send({ message: "Successfully logged in", token: token });
 };
